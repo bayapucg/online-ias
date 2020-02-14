690cba4f4e6baa569f01495fb2b6644c6de98ba0
vasudevareddy <vasudevareddy549@gmail.com>
2020-02-14 17:00:41 +0530

    dashboard  changes

--- application/views/html/dashboard.php
+++ application/views/html/dashboard.php
@@ -1,24 +1,32 @@
-<?php include("header-links.php"); ?>
-<?php include("header.php"); ?>
+<section style="background:#f1f1f1;" >
 
+		
+		<div class="clearfix">&nbsp;</div>
+		<div class="clearfix">&nbsp;</div>
+		<div class="clearfix">&nbsp;</div>
+		<div class="clearfix">&nbsp;</div>
+<main class="page-content">
 
-  <main class="page-content">
-    <div class="container-fluid">
-      <h2>Welcome to Dashboard</h2>
-      
-      
-     
-        
-    </div>
-
-  </main>
+   
+   
+          <div class="panel panel-info">
+          
+            <div class="panel-body">
+              <div class="row justify-content-center d-flex">
+			  
+               <div class=" col-md-8 card py-4 "> 
+				  <div class="container-fluid">
+					  <h2>Welcome to Dashboard</h2>
+					</div>
+                  
+                  
+                </div>
+              </div>
+            </div>
+                
+            
+          </div>
+       </main>
   <!-- page-content" -->
-</div>
+
 </section>
-
-
-
- 
-<?php include("footer-links.php"); ?>
-
-  
