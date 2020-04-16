4b7619ecaf13bc21a6714f8e152c2bf4de1e593b
bayapucg <bayapu.edu004@gmail.com>
2020-04-16 11:43:55 +0530

    SSS
    dsad

---
+++ slova-clan/index.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Slova</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet"><link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container-fluid d-flex ">
+
+     
+		<div class="col-md-8">
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+        
+          <li class="drop-down"><a href="">Product</a>
+            <ul>
+              <li><a href="#">Product 1</a></li>
+              
+              <li><a href="#">Product 2</a></li>
+              <li><a href="#">Product 3</a></li>
+              <li><a href="#">Product 4</a></li>
+      
+            </ul>
+          </li>
+          <li><a href="#">Service</a></li>
+          <li><a href="#">Contact </a></li>
+
+        
+        </ul>
+      </nav>
+	  </div>
+	   <div class="col-md-4 ">
+		<div id="custom-search-input">
+                            <div class="input-group col-md-12">
+                                <input type="text" class="  search-query form-control" placeholder="Search" />
+                                <span class="input-group-btn">
+                                    <button class="btn btn-danger" type="button">
+                                        <span class=" fa fa-search"></span>
+                                    </button>
+                                </span>
+                            </div>
+                        </div>
+      </div>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row justify-content-center">
+	   <div class="col-lg-2 order-1 order-lg-2 hero-img">
+          <img src="assets/img/ban-img.png" class="img-fluid animated" alt="">
+        </div>
+        </div>
+		 <div class="row justify-content-center">
+        <div class="col-lg-12 pt-5 pt-lg-0 order-2 order-lg-1 text-center">
+          <h1>The Slova Clan</h1>
+          <h2>#TheSlovaClan</h2>
+		<h2>Unapologetically she, Audacious skin-care enthusiasts, Working mums, Women of Colour, Self-loving womenfolk -- come about and share your gorgeousness!</h2>
+			<h2>Let’s have conversations #TheSlovaClan is created for all of you that seek and voice out self-actualization. </h2>
+			<h2>Join us to share your experiences with us for we all deserve
+skin-care and self-care.</h2>
+         
+        </div>
+		<div class="col-lg-12 pt-5 pt-lg-0 order-2 order-lg-1 text-center">
+			  <img src="assets/img/heading-text.png" class="img-fluid animated" alt="">
+		</div>
+       <div class="col-lg-12 pt-5 pt-lg-0 order-2 order-lg-1 text-center">
+			 <h1>Rewards</h1>
+		</div> 
+		<div class="col-lg-12 pt-5 pt-lg-0 order-2 order-lg-1 ">
+			 <div><strong>Free Skin Analysis</strong></div>
+			 <div>Understand your skin’s unique needs by analysing your skin with our AI powered Skin Alchemy</div>
+		</div>
+		<div class="col-lg-12 pt-5 pt-lg-0 order-2 order-lg-1 mt-2	">
+			 <div><strong>Free products</strong></div>
+			 <div>Experience the luxe skincare products by Slova. </div>
+		</div>
+		<div class="col-lg-12 pt-5 pt-lg-0 order-2 order-lg-1 mt-2	">
+			 <div><strong>Content Collaboration Opportunity</strong></div>
+			 <div>Co-creation of all content about your experience with Slova.</div>
+		</div>
+		<div class="col-lg-12 pt-5 pt-lg-0 order-2 order-lg-1 mt-2	">
+			 <div><strong>Regram and Repost </strong></div>
+			 <div>Grow your community by getting featured on our page</div>
+		</div>
+		<div class="col-lg-12 pt-5 pt-lg-0 order-2 order-lg-1 mt-2	">
+			 <div><strong>Ambassadorship </strong></div>
+			 <div>Encourage women to weave into skincare and grow to become the ambassadors of Slova.</div>
+		</div>
+		 <div class="col-lg-12 pt-5 pt-lg-0 order-2 order-lg-1 text-center mt-4">
+			 <h1 class="mt-4">How to Participate</h1>
+			 <div class="card" style="background:#d8d8d8;padding:30px 50px;font-size:20px;border-radius:30px;">
+				<div class="row">
+				<div class="col-md-6 mt-3">
+					<img src="assets/img/partc1.png" class="img-fluid " alt="">
+					<div class="text-left">
+							1. Tell us your interest (Dropdown – <br>
+							Give-away, Contest, Vlog, Blog, Reviews
+					</div>
+				</div>
+				<div class="col-md-6 mt-3">
+					<img src="assets/img/partc2.png" class="img-fluid " alt="">
+					<div class="text-left">
+							2. <a href="https://www.slovacosmetics.com/skin-alchemy">https://www.slovacosmetics.com/skin-alchemy</a><br>
+ Register – Snap a Selfie –<br>
+View Product Recommendations 
+					</div>
+				</div>
+				<div class="col-md-6 mt-3">
+					<img src="assets/img/partc3.png" class="img-fluid " alt="">
+					<div class="text-left">
+							3. DM us your product recommendations <br>and your Media Kit on Instagram
+					</div>
+				</div>
+				<div class="col-md-6 mt-3">
+					<img src="assets/img/partc4.png" class="img-fluid " alt="">
+					<div class="text-left">
+							4. Get free products and share<br>
+your reviews with your community! 
+					</div>
+				</div>
+			 </div>
+		</div>
+       
+      </div>
+	  <div class="col-lg-12 pt-5 pt-lg-0 order-2 order-lg-1 mt-2	">
+		<iframe width="100%" height="450" src="https://www.youtube.com/embed/a3ICNMQW7Ok" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+		</div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <section id="services" class="services ">
+      <div class="container" style="background:#ededed;padding:150px 50px 50px 50px;border-radius:35px;">
+
+
+        <div class="row">
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <div  class="test-img " style=""><img src="assets/img/t1.png" class="img-fluid " alt=""></div>
+              <h4 class="title mt-4"><a href="">Lorem Ipsum</a></h4>
+              <p class="description">"Lorem Ipsum Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate"</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box">
+              <div  class="test-img " style=""><img src="assets/img/t1.png" class="img-fluid " alt=""></div>
+              <h4 class="title mt-4"><a href="">Lorem Ipsum</a></h4>
+              <p class="description">"Lorem Ipsum Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate"</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
+             <div class="icon-box">
+              <div  class="test-img " style=""><img src="assets/img/t1.png" class="img-fluid " alt=""></div>
+              <h4 class="title mt-4"><a href="">Lorem Ipsum</a></h4>
+              <p class="description">"Lorem Ipsum Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate"</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="400">
+             <div class="icon-box">
+              <div  class="test-img " style=""><img src="assets/img/t1.png" class="img-fluid " alt=""></div>
+              <h4 class="title mt-4"><a href="">Lorem Ipsum</a></h4>
+              <p class="description">"Lorem Ipsum Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate"</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+    <section>
+	<div class="container">
+	<div class="row">
+		<div class="col-lg-12 pt-5 pt-lg-0 order-2 order-lg-1 text-center">
+			 <h1>Quick Questions</h1>
+		</div> 
+		<div class="col-lg-12 pt-5 pt-lg-0 order-2 order-lg-1 ">
+			 <div><strong>Who can take part?</strong></div>
+			 <div>Airhead, Ambitious, Abrasive, Bossy, Bitchy, Bubbly, Ditsy, Emotional, Frigid,
+Frumpy, High-maintenance, Pushy, Sassy, Bombshell, Bolshy, Breathless, Bridezilla,
+Curvy, Ethereal, Fawn-like, Headstrong, Hormonal, Hysterical, Illogical, Shrill, Voluptuous, Whinging, Working mum</div>
+		</div>
+		<div class="col-lg-12 pt-5 pt-lg-0 order-2 order-lg-1 mt-2	">
+			 <div><strong>How many followers do I need to have?</strong></div>
+			 <div>Slova values genuine intent to voice out your support for self-love. All original and
+creative content is valued regardless of the follower count. </div>
+		</div>
+		<div class="col-lg-12 pt-5 pt-lg-0 order-2 order-lg-1 mt-2	">
+			 <div><strong>Which category of influencers can apply?</strong></div>
+			 <div>Skincare enthusiasts, self-love proclaimers, women of all races, ethnicities, religions,
+gender identities, body types, sexual orientations, and abilities are welcome to apply.</div>
+		</div>
+		<div class="col-lg-12 pt-5 pt-lg-0 order-2 order-lg-1 mt-2	">
+			 <div><strong>Is this open globally?</strong></div>
+			 <div>Oh, yes! C’mon send in your details.</div>
+		</div>
+		</div>
+		</div>
+    </section>
+
+   
+    <!-- ======= Contact Us Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Contact Us</h2>
+         
+        </div>
+
+        <div class="row">
+
+        
+
+          <div class="col-lg-12 mt-5 mt-lg-0 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="form-row">
+                <div class=" col-md-6">
+                <div class="form-group ">
+                  <label for="name">Your Name</label>
+                  <input type="text" name="name" class="form-control" id="name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                  <div class="validate"></div>
+                </div>
+				<div class="form-group ">
+                  <label for="name">Your Email</label>
+                  <input type="email" class="form-control" name="email" id="email" data-rule="email" data-msg="Please enter a valid email" />
+                  <div class="validate"></div>
+                </div>
+				 <div class="form-group">
+                <label for="name">Mobile</label>
+                <input type="text" class="form-control" name="subject" id="subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                <div class="validate"></div>
+              </div>
+                </div>
+                <div class="form-group col-md-6">
+                <label for="name">Message</label>
+                <textarea class="form-control" name="message" rows="10" data-rule="required" data-msg="Please write something for us"></textarea>
+                <div class="validate"></div>
+              </div>
+              </div>
+             
+              
+              <div class="mb-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Us Section -->
+
+  </main>
+   <footer id="footer" style="background:#999999;">
+
+  
+
+   
+
+    <div class="container py-4">
+      <div class="row justify-content-center">
+	   <div class="col-lg-2 order-1 order-lg-2 hero-img">
+          <img src="assets/img/social.png" class="img-fluid animated" alt="">
+        </div>
+		
+        </div>
+		<div class="row">
+	  <div class="col-lg-12 order-1 order-lg-2 text-center">
+          <h1>@slovacosmetics</h1>
+        </div>
+		
+        </div>
+    </div>
+  </footer>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
