42593fec860958c27ecf45ce0bf24c530110379a
vasudevareddyreddem <vasudevareddy549@gmail.com>
2020-05-04 11:09:35 +0530

    Add files via upload

---
+++ b2b_ui_changes.php
@@ -0,0 +1,3 @@
+ http://dev.freyrsolutions.com/demo/covid-19/covid-home.html
+http://dev.freyrsolutions.com/demo/covid-19/Product-Country-Page.html
+http://dev.freyrsolutions.com/demo/covid-19/Product-Country-Page-02.html
