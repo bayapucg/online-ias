50179ff08256212886b7cdad99bbba2573dff2e2
vasudevareddyreddem <vasudevareddy549@gmail.com>
2020-05-12 08:21:51 +0530

    Update b2b_ui_changes.php

--- b2b_ui_changes.php
+++ b2b_ui_changes.php
@@ -3,3 +3,9 @@ http://dev.freyrsolutions.com/demo/covid-19/Product-Country-Page.html
 http://dev.freyrsolutions.com/demo/covid-19/Product-Country-Page-02.html
 
 https://freyrsolutions-my.sharepoint.com/personal/rohit_nyathawada_freyrsolutions_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Frohit%5Fnyathawada%5Ffreyrsolutions%5Fcom%2FDocuments%2FTaevas%20Life%20Sciences%2FB2B%20Portal%20Products%20and%20Images&originalPath=aHR0cHM6Ly9mcmV5cnNvbHV0aW9ucy1teS5zaGFyZXBvaW50LmNvbS86ZjovcC9yb2hpdF9ueWF0aGF3YWRhL0V2RUh0emstTlJKSWhvVlBIYlk4X1pjQlpfd0ZrOVJOSjBiQjhHZVdZQkp4QkE_cnRpbWU9N0FVcjl3X3cxMGc
+
+
+
+
+Safety & Medical products 
+Freyr Safety products portfolio includes protective clothing, gloves, masks, eyewear and essential medical equipment dedicated to your safety and comfort 
